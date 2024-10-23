7f4f7adb353968aca00d6ff8328c9d87514163d8
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-10-23 13:23:51 -0600

    updated the browse book section to have more files and better separation and better laoding

--- frontend/src/app/browse/page.jsx
+++ frontend/src/app/browse/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import S3Imags from '@/components/general/S3Imgs';
-import BookList from '@/components/library/DisplayAllBooks';
+import BookList from '@/components/library/browse/AllBooks';
 
 function BooksPage() {
   return (
