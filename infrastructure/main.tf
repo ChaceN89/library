28c59e6839368eee14bdc0d62492dda768f6d59a
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-01-07 13:01:57 -0700

    set up s3 buckets with internal folders

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,3 +23,26 @@ resource "aws_s3_bucket" "library_app_s3_bucket" {
     Environment = var.environment
   }
 }
+
+# Create folders in the S3 bucket (folders are just "keys" with a trailing "/")
+resource "aws_s3_object" "folders" {
+  for_each = toset(["bookArt/", "books/", "frontendAssets/", "misc/", "profilePictures/"])
+  bucket   = aws_s3_bucket.library_app_s3_bucket.id
+  key      = each.value
+}
+
+# Upload files from the local frontendAssets folder to the S3 bucket
+resource "aws_s3_object" "frontend_assets" {
+  for_each = fileset("${path.module}/s3/frontendAssets", "*")
+  bucket   = aws_s3_bucket.library_app_s3_bucket.id
+  key      = "frontendAssets/${each.value}"
+  source   = "${path.module}/s3/frontendAssets/${each.value}"
+}
+
+# Upload files from the local misc folder to the S3 bucket
+resource "aws_s3_object" "misc_files" {
+  for_each = fileset("${path.module}/s3/misc", "*")
+  bucket   = aws_s3_bucket.library_app_s3_bucket.id
+  key      = "misc/${each.value}"
+  source   = "${path.module}/s3/misc/${each.value}"
+}
