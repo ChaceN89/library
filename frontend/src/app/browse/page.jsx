a19df6a189982823339f0574724e447ef0d974b5
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-01-10 15:48:48 -0700

    got book card style right

--- frontend/src/app/browse/page.jsx
+++ frontend/src/app/browse/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Link from 'next/link';
-import S3Imags from '@/components/general/S3Imgs';
+
 import BookList from '@/components/library/browse/AllBooks';
 
 function BooksPage() {
--- frontend/src/components/home/BookCard.jsx
+++ frontend/src/components/home/BookCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import SetFavBook from '../library/favBooks/SetFavBook';
+import BookCardTitle from './BookCardTitle';
 import { bookCardData } from '@/data/bookCardData';
 
 function BookCard({ book }) {
@@ -13,10 +13,10 @@ function BookCard({ book }) {
       onClick={() => (window.location.href = bookLink)} // Navigate to book link when clicking anywhere on the card
       className="bg-gray-100 border border-gray-300 rounded-lg p-4 mb-4 shadow-lg flex flex-col md:flex-row gap-4 cursor-pointer hover:shadow-xl transition-shadow"
     >
-      {/* Book Cover Image */}
-      <div
-        className="w-full md:w-1/3 aspect-[3/4] bg-gray-300 rounded-lg overflow-hidden relative flex-shrink-0"
-      >
+      {/* Title for Smal Screens */}
+      <BookCardTitle book={book} showOnSmallScreens={true} showOnLargeScreens={false} />
+
+      <div className="w-full md:w-1/3 aspect-[3/4] bg-gray-300 rounded-lg overflow-hidden relative flex-shrink-0">
         <Image
           src={book.cover_art_url || bookCardData.defaultImg}
           alt={`Cover art for ${book.title || 'Default Cover'}`}
@@ -32,13 +32,8 @@ function BookCard({ book }) {
         className="flex flex-col flex-grow gap-2 md:w-2/3"
         onClick={(e) => e.stopPropagation()} // Prevent card navigation when interacting with internal elements
       >
-        {/* Title and Favorite Button */}
-        <div className="flex justify-between items-start">
-          <h4 className="text-lg font-bold text-blue-600 hover:underline cursor-pointer flex-grow">
-            {book.title}
-          </h4>
-          <SetFavBook id={book.id} />
-        </div>
+        {/* Title for Large Screens */}
+        <BookCardTitle book={book} showOnSmallScreens={false} showOnLargeScreens={true} />
 
         {/* Book Author */}
         <p className="text-gray-600 text-sm font-semibold">{book.author}</p>
@@ -49,6 +44,7 @@ function BookCard({ book }) {
         <p className="text-gray-600 text-sm">
           Last updated: {new Date(book.updated_at).toLocaleDateString()}
         </p>
+
       </div>
     </li>
   );
