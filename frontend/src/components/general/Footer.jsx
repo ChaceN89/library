a2bb71dbdd3d4277617881c072ddab9751225bdb
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-01-11 16:55:11 -0700

    updated footer

--- frontend/src/components/general/Footer.jsx
+++ frontend/src/components/general/Footer.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+/**
+ * @file Footer.jsx
+ * @module Footer
+ * @description 
+ *   A simple footer component for the application. Includes a logo link to navigate back to the top of the page 
+ *   and a reference to the author's website for contact.
+ *
+ * @example
+ *   <Footer />
+ * 
+ * @author Chace Nielson
+ * @created 2025-01-10
+ * @updated 2025-01-11
+ */
+
+import React from "react";
+import LogoLink from "./LogoLink"; // Logo link for navigation
 
 function Footer() {
   return (
-    <div className='bg-red-800 w-full h-10 '>
-      <div className='container mx-auto'>
-       Footer
+    <footer className="bg-accent flex relative bottom-0">
+      <div className="container mx-auto flex justify-between items-center p-1 sm:p-2">
+        {/* Link back to the top with the logo */}
+        <LogoLink />
 
+        {/* Author/contact link */}
+        <h5 className="text-primary hover:underline hover:cursor-pointer" >
+          <a 
+            href="https://chacenielson.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Chace Nielson
+          </a>
+        </h5>
       </div>
-    </div>
-  )
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
